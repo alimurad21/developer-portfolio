208fc3560db0f48adf3ede95d43ec28552415a33
alimurad21 <alimurad3422@gmail.com>
2024-10-26 09:33:19 +0500

    Project updated

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'FullStack Developer',
+    company: "HiSkyTech (Part time)",
+    duration: "(Jun 2024 - Sept 2024)"
   },
   {
     id: 2,
     title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    company: "Upword (Freelance)",
+    duration: "(Jun 2022 - Present)"
   },
   {
     id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    duration: "(Oct 2021 - Present)"
   }
 ]
